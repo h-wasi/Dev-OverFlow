5fa780cf10e94d8333581c032929224fc8e48a28
h-wasi <akhsaikas@gmail.com>
2023-11-03 16:04:09 +0530

    1

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -66,6 +66,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate"), require("tailwindcss/typography")],
+  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
 };
 export default config;
