dcc82190560e902983687dbc917281b71b3a4409
h-wasi <akhsaikas@gmail.com>
2023-11-11 16:05:50 +0530

    profile link is attached to sign in condition

--- components/shared/leftsidebar/Sidebar.tsx
+++ components/shared/leftsidebar/Sidebar.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 // import { Button, buttonVariants } from "@/components/ui/button";
-import { SignedOut } from "@clerk/nextjs";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { SidebarLink } from "@/constants";
 import { sidebarLinks } from "@/constants/constant";
 import { usePathname } from "next/navigation";
@@ -13,12 +13,12 @@ function LSidebar() {
   const pathname = usePathname();
   return (
     <section className="custom-scrollbar background-light900_dark200 light-border sticky left-0 top-0 flex h-screen w-fit flex-col justify-between  overflow-y-auto border-r p-6 pt-36 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266px]">
-      <div className="flex flex-col justify-between gap-6">
+      <div className="flex flex-col justify-center gap-4">
         {sidebarLinks.map((item) => {
           const isActive =
             (pathname.includes(item.route) && item.route.length > 1) ||
             pathname === item.route;
-          return (
+          return item.label !== "Profile" ? (
             <Link
               href={item.route}
               key={item.label}
@@ -31,14 +31,37 @@ function LSidebar() {
               <Image
                 className={`${isActive ? "" : "invert-colors"}`}
                 src={item.imgURL}
-                width={24}
-                height={24}
+                width={20}
+                height={20}
                 alt={item.label}
               ></Image>
               <p className={`${isActive ? "base-bold" : "base-medium "}`}>
                 {item.label}
               </p>
             </Link>
+          ) : (
+            <SignedIn>
+              <Link
+                href={item.route}
+                key={item.label}
+                className={`${
+                  isActive
+                    ? "primary-gradient rounded-lg text-light-900"
+                    : "text-dark300_light900"
+                } flex items-center justify-start gap-4 bg-transparent p-4`}
+              >
+                <Image
+                  className={`${isActive ? "" : "invert-colors"}`}
+                  src={item.imgURL}
+                  width={20}
+                  height={20}
+                  alt={item.label}
+                ></Image>
+                <p className={`${isActive ? "base-bold" : "base-medium "}`}>
+                  {item.label}
+                </p>
+              </Link>
+            </SignedIn>
           );
         })}
       </div>
@@ -52,7 +75,7 @@ function LSidebar() {
           </Link>
 
           <Link href={"/sign-up"}>
-            <Button className="small-medium light-border-2 btn-tertiary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none text-dark400_light900">
+            <Button className="small-medium light-border-2 btn-tertiary min-h-[40px] w-full rounded-lg px-4 py-3 shadow-none text-dark400_light900">
               Sign Up
             </Button>
           </Link>
